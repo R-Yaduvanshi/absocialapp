47f4238b7bc9ebd0f83708a141db51d41701e72f
RAJENDRA YADAV <rjyadav0230@gmail.com>
2023-04-11 12:48:04 +0530

    Top 5 active user part done

--- frontend/src/components/UserAnalytics.jsx
+++ frontend/src/components/UserAnalytics.jsx
@@ -19,13 +19,20 @@ import {
   AiFillFacebook,
 } from "react-icons/ai";
 const getTotalUserNumber = async () => {
-  let res = await axios.get("http://localhost:7000/analytics/users");
+  const res = await axios.get("http://localhost:7000/analytics/users");
+  return res;
+};
+
+const getActiveUser = () => {
+  const res = axios.get("http://localhost:7000/analytics/users/top-active");
   return res;
 };
 const UserAnalytics = () => {
   const [totalUsers, setTotalUsers] = useState("");
+  const [activeUser, setActiveUser] = useState([]);
   useEffect(() => {
     getTotalUserNumber().then((res) => setTotalUsers(res.data.total));
+    getActiveUser().then((res) => setActiveUser(res.data));
   }, []);
   return (
     <Box p="10px" minH={"88vh"} mt="12vh">
@@ -53,326 +60,71 @@ const UserAnalytics = () => {
         </Text>
       </Container>
       <Box p="20px">
-        <Text textAlign={"center"}>Top 5 Users</Text>
+        <Text
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize={["20px", "20px", "28px", "", ""]}
+          fontWeight="extrabold"
+          textAlign={"center"}
+        >
+          Top 5 Users
+        </Text>
         <SimpleGrid columns={3} spacing={5} mt="10px">
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
+          {activeUser?.map((el) => {
+            return (
+              <Box
+                minH={"350px"}
+                maxH={"auto"}
+                bgColor={"#8785A2"}
+                boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
+                borderRadius={"97.7021px"}
+                p="20px"
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
               >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
-              >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
-              >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
-              >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
-              >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
-          <Box
-            // maxW={"30%"}
-            h="370px"
-            bgColor={"#8785A2"}
-            boxShadow={"0px 8.7234px 20.9362px rgba(32, 31, 41, 0.08);"}
-            borderRadius={"97.7021px"}
-            p="20px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box w="80%" h="310px">
-              <Flex display={"flex"} justifyContent={"center"}>
-                <Avatar
-                  size="2xl"
-                  name="Segun Adebayo"
-                  src="https://bit.ly/sage-adebayo"
-                />
-              </Flex>
-              <Text
-                textAlign={"center"}
-                fontWeight={"700"}
-                color={"#F6F6F6"}
-                fontSize={"24px"}
-              >
-                Virat Kohli
-              </Text>
-              <Text
-                textAlign={"center"}
-                fontWeight={"400"}
-                color={"#F6F6F6"}
-                fontSize={"18px"}
-              >
-                virat@gmail.com
-              </Text>
-              <Text
-                color={"#F6F6F6"}
-                fontSize={"12px"}
-                textAlign={"center"}
-                mt="10px"
-                fontWeight={"600"}
-              >
-                Shambhavi is a well-rounded web publisher with 15 years
-                experience working as an art director for large corporations in
-                the technology sector.
-              </Text>
-              <Flex justifyContent={"center"} gap="10px" mt="10px">
-                <Icon as={AiOutlineInstagram}></Icon>
-                <Icon as={AiOutlineTwitter}></Icon>
-                <Icon as={AiFillFacebook}></Icon>
-              </Flex>
-            </Box>
-          </Box>
+                <Box w="80%" h="310px">
+                  <Flex display={"flex"} justifyContent={"center"}>
+                    <Avatar
+                      size="xl"
+                      name="Segun Adebayo"
+                      src="https://xsgames.co/randomusers/avatar.php?g=pixel"
+                    />
+                  </Flex>
+                  <Text
+                    textAlign={"center"}
+                    fontWeight={"700"}
+                    color={"#F6F6F6"}
+                    fontSize={"24px"}
+                  >
+                    {el.name}
+                  </Text>
+                  <Text
+                    textAlign={"center"}
+                    fontWeight={"400"}
+                    color={"#F6F6F6"}
+                    fontSize={"18px"}
+                  >
+                    {el.email}
+                  </Text>
+                  <Text
+                    color={"#F6F6F6"}
+                    fontSize={"12px"}
+                    textAlign={"center"}
+                    mt="10px"
+                    fontWeight={"600"}
+                  >
+                    {el.bio}
+                  </Text>
+                  <Flex justifyContent={"center"} gap="10px" mt="10px">
+                    <Icon as={AiOutlineInstagram}></Icon>
+                    <Icon as={AiOutlineTwitter}></Icon>
+                    <Icon as={AiFillFacebook}></Icon>
+                  </Flex>
+                </Box>
+              </Box>
+            );
+          })}
         </SimpleGrid>
       </Box>
     </Box>
